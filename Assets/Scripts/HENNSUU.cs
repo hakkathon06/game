67c9c46ac930467b4dec5bc767efdadcdf910ff0
pikachu0310 <sunnon@i.softbank.jp>
2022-06-19 00:17:44 +0900

    ちょっと変更

--- Assets/Scripts/HENNSUU.cs
+++ Assets/Scripts/HENNSUU.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HENNSUU : MonoBehaviour
 {
-    [SerializeField] public float speed = 1;
+    public float speed = 14f;
     [SerializeField] public float CreateItemInterval = 1;
     [SerializeField] public float CreateHurdleInterval = 1;
     // Start is called before the first frame update
@@ -16,6 +16,6 @@ public class HENNSUU : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = 14f * Parameter.Instance().strength;
     }
 }
